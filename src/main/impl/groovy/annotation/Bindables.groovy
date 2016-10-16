05bd3cf2525b8e06e98d29b659ec704359d9fbed
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-10-16 11:34:13 +0100

    Spread operator

--- src/main/impl/groovy/annotation/Bindables.groovy
+++ src/main/impl/groovy/annotation/Bindables.groovy
@@ -4,10 +4,10 @@ import groovy.beans.Bindable
 import org.junit.Test
 
 /**
- * Created by pabloperezgarcia on 15/10/2016.
+ * Bindable annotation allow us provide a listener into class or attribute level that allow us for instance
+ * detect when the class/attributes are modified, invoking the callback that we define in addPropertyChangeListener
  */
 class Bindables {
-
 
     @Bindable
     class User {
@@ -16,17 +16,17 @@ class Bindables {
 
     @Test
     def void testBindables() {
-        def u = new User(name: 'mrhaki', email: 'mrhaki@mrhaki.com')
+        def u = new User(name: 'Pablo', email: 'pablo@gmail.com')
 
         u.addPropertyChangeListener { event ->
             println "Changed property $event.propertyName from $event.oldValue to $event.newValue"
         }
 
-        u.name = 'Hubert A. Klein Ikkink'
-        // Output: Changed property name from mrhaki to Hubert A. Klein Ikkink
+        u.name = 'Mr Nobody'
+        // Output: Changed property name from Pablo to Mr Nobody
 
-        u.email = 'hubert@mrhaki.com'
-        // Output: Changed property email from mrhaki@mrhaki.com to hubert@mrhaki.com
+        u.email = 'nobody@gmail.com.com'
+        // Output: Changed property email from pablo@gmail.com to nobody@gmail.com.com
 
     }
 
